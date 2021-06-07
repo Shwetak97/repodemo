0a14c52a01c83b5f8fdf842501ddff09da094fb8
Shweta Kale <shwetakale25797@gmail.com>
2021-06-07 14:45:58 +0530

    create repo

---
+++ simplepgrm/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simplepgrm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1, Number2;
+
+            Console.WriteLine("please enter the Number1");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter the Number2");
+            Number2 = Convert.ToInt32(Console.ReadLine());
+
+            int Result;
+            Result = Number1 + Number2;
+
+            Console.WriteLine("Sum of two Numbers:" + Result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
